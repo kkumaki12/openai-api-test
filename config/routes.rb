bf9113c825f2a890731c0cb2d59310eb406d3e10
kkumaki12 <79786417+kkumaki12@users.noreply.github.com>
2023-04-08 11:27:33 +0900

    rails generate controller Questions index show new create

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'questions/index'
+  get 'questions/show'
+  get 'questions/new'
+  get 'questions/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
