65a287061a7a8721afce6353dec32ae1a7ad2751
kkumaki12 <79786417+kkumaki12@users.noreply.github.com>
2023-04-08 12:43:54 +0900

    コントローラ作成

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,42 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only: [:show]
+
   def index
+    @questions = Question.all
   end
 
   def show
+    @question = Question.find(params[:id])
+    @answer = @question.answers.build
   end
 
   def new
+    @question = Question.new
   end
 
   def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
+  def generate_text
+    prompt = params[:prompt]
+    generated_text = ChatGptService.generate_text(prompt)
+    render json: { generated_text: generated_text }
+  end
+
+  private
+
+  def set_question
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:title, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'answers/create'
-  get 'questions/index'
-  get 'questions/show'
-  get 'questions/new'
-  get 'questions/create'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :questions do
+    resources :answers, only: [:create]
+    post 'generate_text', on: :collection
+  end
+  root "questions#index"
 end
