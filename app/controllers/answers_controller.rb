1bd805e19226e9f6dff050453fdfb680ad9eccb5
kkumaki12 <79786417+kkumaki12@users.noreply.github.com>
2023-04-08 12:44:26 +0900

    フォーム修正

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,16 @@
 class AnswersController < ApplicationController
   def create
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.create(answer_params)
+    respond_to do |format|
+      format.html { redirect_to @question }
+      format.js
+    end
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:content)
   end
 end
